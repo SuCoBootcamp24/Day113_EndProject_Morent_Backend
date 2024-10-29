feeb54fb018a54740cc0d921573e1dd7f8503c69
René Behrens <rene.behrens@aol.com>
2024-10-29 15:49:39 +0100

    Working on the JWT implementation

--- Backend/src/main/java/de/morent/backend/BackendApplication.java
+++ Backend/src/main/java/de/morent/backend/BackendApplication.java
@@ -1,9 +1,12 @@
 package de.morent.backend;
 
+import de.morent.backend.configurations.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class BackendApplication {
 
     public static void main(String[] args) {
