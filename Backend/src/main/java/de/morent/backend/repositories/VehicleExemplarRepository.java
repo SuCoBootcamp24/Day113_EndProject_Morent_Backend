62268929e7ccd5300782791961327a44f7de3ff2
René Behrens <rene.behrens@aol.com>
2024-10-29 18:10:50 +0100

    Update VehicleExemplarRepository.java

--- Backend/src/main/java/de/morent/backend/repositories/VehicleExemplarRepository.java
+++ Backend/src/main/java/de/morent/backend/repositories/VehicleExemplarRepository.java
@@ -1,8 +1,9 @@
 package de.morent.backend.repositories;
 
+import de.morent.backend.entities.VehicleExemplar;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VehicleExemplarRepository extends JpaRepository<VehicleExemplarRepository, Long> {
+public interface VehicleExemplarRepository extends JpaRepository<VehicleExemplar, Long> {
 }
