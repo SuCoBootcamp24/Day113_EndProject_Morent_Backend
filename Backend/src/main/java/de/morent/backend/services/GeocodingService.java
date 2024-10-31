ce78ec256f90d12615aac278eae1df32012f8c17
René Behrens <rene.behrens@aol.com>
2024-10-31 21:23:29 +0100

    creat calcdistance methods

--- Backend/src/main/java/de/morent/backend/services/GeocodingService.java
+++ Backend/src/main/java/de/morent/backend/services/GeocodingService.java
@@ -1,6 +1,8 @@
 package de.morent.backend.services;
 
 import de.morent.backend.entities.Address;
+import org.springframework.data.geo.Metric;
+import org.springframework.data.geo.Metrics;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -11,9 +13,11 @@ import java.util.Map;
 @Service
 public class GeocodingService {
     private final RestTemplate restTemplate;
+    private RedisService redisService;
 
-    public GeocodingService(RestTemplate restTemplate) {
+    public GeocodingService(RestTemplate restTemplate, RedisService redisService) {
         this.restTemplate = restTemplate;
+        this.redisService = redisService;
     }
 
 
@@ -39,8 +43,7 @@ public class GeocodingService {
                 .queryParam("addressdetails", "1")
                 .toUriString();
 
-        //String url = "https://nominatim.openstreetmap.org/search?q=Berner%20heerweg%20151,%20Hamburg,%2022159,%20Deutschland&format=xml&polygon=1&addressdetails=1";
-        System.out.println(url);
+         System.out.println(url);
         List<Map<String, Object>> response = restTemplate.getForObject(url, List.class);
 
         System.out.println(response);
@@ -54,5 +57,29 @@ public class GeocodingService {
     }
 
 
+    public void calcDistance(String fromname, String fromAddress,String toname, String toAddress) {
+        double [] fromcoords = extracted(fromAddress);
 
+        double [] tocoords = extracted(toAddress);
+
+        redisService.addLocation(fromname, fromcoords[0], fromcoords[1]);
+        redisService.addLocation(toname, tocoords[0], tocoords[1]);
+
+        Double distance = redisService.getDistance(fromname, toname, Metrics.KILOMETERS);
+        System.out.println(distance);
+
+    }
+
+    private double [] extracted(String fromAddress) {
+        String[] fromParts = fromAddress.split(",");
+        if (fromParts.length != 2) {
+            throw new IllegalArgumentException("Invalid coordinate format. Anticipated: 'latitude, longitude'.");
+        }
+        try {
+            double fromAddLat = Double.parseDouble(fromParts[0].trim());
+            double fromAddlon = Double.parseDouble(fromParts[1].trim());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Koordinaten konnten nicht geparst werden.", e);
+        }
+    }
 }
