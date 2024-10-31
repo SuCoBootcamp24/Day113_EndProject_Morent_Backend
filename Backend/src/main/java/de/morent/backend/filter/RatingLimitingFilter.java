b0dcc04491274e72d186584b3f9ff3595f87cd81
andreasReichHl <reich.andreas.hl@gmail.com>
2024-10-31 01:23:46 +0100

    RateLimitingRequest Filter implemented

---
+++ Backend/src/main/java/de/morent/backend/filter/RatingLimitingFilter.java
@@ -0,0 +1,51 @@
+package de.morent.backend.filter;
+
+import de.morent.backend.services.RedisService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class RatingLimitingFilter extends OncePerRequestFilter {
+    private final RedisService redisService;
+    private final Integer MAX_REQUESTS = 5;
+    private final long TIMEOUT_MINUTES = 2;
+
+    public RatingLimitingFilter(RedisService redisService) {
+        this.redisService = redisService;
+    }
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String clientId = request.getRemoteAddr();
+
+        if (isRateLimitExceeded(clientId)) {
+            response.setStatus(HttpStatus.TOO_MANY_REQUESTS.value());
+            response.getWriter().write("Too Many Requests - Rate limit of " + MAX_REQUESTS + " exceeded.");
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+
+    public boolean isRateLimitExceeded(String clientId) {
+        Long countValue = redisService.getClientRequestCount(clientId);
+        if (countValue == null) {
+            redisService.saveClientRequestCount(clientId, 1L, TIMEOUT_MINUTES);
+            countValue = 1L;
+        } else {
+            redisService.saveClientRequestCount(clientId, countValue + 1L, TIMEOUT_MINUTES);
+        }
+        return redisService.getClientRequestCount(clientId) >= MAX_REQUESTS;
+    }
+
+
+}
