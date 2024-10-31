e440e3829a42b284a6d3ff54e16d10bb5f5832b8
VittorioDeMarzi <vittoriodm7@gmail.com>
2024-10-31 14:19:23 +0100

    add GlobalExceptionHandler and VehicleDtoMapper

---
+++ Backend/src/main/java/de/morent/backend/mappers/VehicleMapper.java
@@ -0,0 +1,36 @@
+package de.morent.backend.mappers;
+
+import de.morent.backend.dtos.vehicle.VehicleDTO;
+import de.morent.backend.entities.Vehicle;
+
+public class VehicleMapper {
+
+/*    public static Vehicle mapToEntity(VehicleRequestDTO dto) {
+        return new Vehicle(
+                dto.carType(),
+                dto.brand(),
+                dto.model(),
+                dto.seats(),
+                dto.engineCapacity(),
+                dto.isAutomatic(),
+                dto.fuelType(),
+                dto.img(),
+                dto.consumption()
+        );
+    }*/
+
+    public static VehicleDTO mapToDto(Vehicle vehicle) {
+        return new VehicleDTO(
+                vehicle.getId(),
+                vehicle.getCarType(),
+                vehicle.getBrand(),
+                vehicle.getModel(),
+                vehicle.getSeats(),
+                vehicle.getEngineCapacity(),
+                vehicle.isAutomatic(),
+                vehicle.getFuelType(),
+                vehicle.getImage().getThumbnailUrl(),
+                vehicle.getConsumption()
+        );
+    }
+}
