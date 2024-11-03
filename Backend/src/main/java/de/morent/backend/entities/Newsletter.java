5f0c13c7a12a243eef7fa146d7c7822146f9961c
René Behrens <rene.behrens@aol.com>
2024-11-03 17:59:34 +0100

    create AddCustomerToNewsletter

--- Backend/src/main/java/de/morent/backend/entities/Newsletter.java
+++ Backend/src/main/java/de/morent/backend/entities/Newsletter.java
@@ -1,18 +1,21 @@
 package de.morent.backend.entities;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Newsletter {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
 
-    String firsnName;
+    String firstName;
 
     String lastName;
 
+    @Column(unique = true)
     String email;
 
     boolean isRegistered = false;
--- Backend/src/main/java/de/morent/backend/repositories/NewsRepository.java
+++ Backend/src/main/java/de/morent/backend/repositories/NewsRepository.java
@@ -3,5 +3,8 @@ package de.morent.backend.repositories;
 import de.morent.backend.entities.Newsletter;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface NewsRepository extends CrudRepository<Newsletter, Long> {
+    Optional<Newsletter> findByEmail(String email);
 }
