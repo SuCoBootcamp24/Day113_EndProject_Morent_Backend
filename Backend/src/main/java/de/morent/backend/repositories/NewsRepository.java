44db444ec45da9dd86c37fc0ff195cbff3e64ce8
René Behrens <rene.behrens@aol.com>
2024-11-03 19:24:41 +0100

    create Del and Sendmail endpoint

--- Backend/src/main/java/de/morent/backend/repositories/NewsRepository.java
+++ Backend/src/main/java/de/morent/backend/repositories/NewsRepository.java
@@ -2,9 +2,11 @@ package de.morent.backend.repositories;
 
 import de.morent.backend.entities.Newsletter;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface NewsRepository extends CrudRepository<Newsletter, Long> {
     Optional<Newsletter> findByEmail(String email);
 }
